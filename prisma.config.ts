8d506bbac3f12625e654dc54c684ff62d06bbc6f
a.rsyad96.242 <a.rsyad96.242@gmail.com>
2026-08-03 00:24:49 +0800

    monitor

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
